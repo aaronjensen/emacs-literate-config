5e858172c47da53d4eaf738592175f6b1c0b8435
Aaron Jensen <aaronjensen@gmail.com>
2024-10-30 23:23:06 -0700

    Ensuring compilation, hooks, and loading

--- literate-config.el
+++ literate-config.el
@@ -25,23 +25,25 @@
 
 ;;; Code:
 
+(defvar literate-config-org-file-name (expand-file-name (concat user-emacs-directory "config.org")))
+(defvar literate-config-el-file-name (expand-file-name (concat user-emacs-directory "config.el")))
 (defvar literate-config-before-section-hook nil)
 
 ;;;###autoload
-(defun literate-config-compile-config ()
-  "This function will write all source blocks from =config.org= into =config.el= that:
+(defun literate-config-compile ()
+  "Write all source blocks from =config.org= into =config.el= that:
 - Are not marked as `tangle: no'
 - Don't have the TODO state `DISABLED'
 - Have a source-code of `emacs-lisp'"
   (require 'org)
   (let* ((body-list ())
-         (output-file (concat user-emacs-directory "config.el"))
+         (output-file literate-config-el-file-name)
          (org-babel-default-header-args (org-babel-merge-params org-babel-default-header-args
                                                                 (list (cons :tangle output-file)))))
-    (let ((inhibit-message t)) (message "Generating %s" output-file))
+    (let ((inhibit-message t)) (message "Generating %s..." output-file))
     (save-restriction
       (save-excursion
-        (org-babel-map-src-blocks (concat user-emacs-directory "config.org")
+        (org-babel-map-src-blocks literate-config-org-file-name
           (let* ((org_block_info (org-babel-get-src-block-info 'light))
                  (tfile (cdr (assq :tangle (nth 2 org_block_info))))
                  (todo-keyword-match))
@@ -58,17 +60,45 @@
               (add-to-list 'body-list (concat "\n\n;; #############################################################\n"
                                               ";; " (org-get-heading) "\n"
                                               ";; #############################################################\n\n"
-                                              "(run-hook-with-args 'literate-config-before-section-hook \"" (org-get-heading) "\")\n"))
+                                              "(run-hook-with-args 'literate-config-before-section-hook \"" (org-get-heading) "\")\n\n"))
               (add-to-list 'body-list body)))))
       (with-temp-file output-file
         (insert ";;; config.el -*- lexical-binding: t; byte-compile-warnings: (not free-vars make-local noruntime); -*-\n")
         (insert ";; ============================================================\n")
-        (insert ";; Don't edit this file, edit config.org instead.\n")
+        (insert ";; Don't edit this file, edit org config instead.\n")
         (insert ";; Generated at " (format-time-string "%a %b %d %Y-%m-%dT%H:%M:%S " (current-time)) "on host " system-name "\n")
         (insert ";; ============================================================\n")
         (insert (apply 'concat (reverse body-list)))
         (insert "\n"))
       (let ((inhibit-message t)) (message "Wrote %s" output-file)))))
+
+;;;###autoload
+(defun literate-config-ensure-compiled ()
+  "Compile the config if it does not exist or the source config has changed."
+  (when (or (not (file-exists-p literate-config-el-file-name))
+            (file-newer-than-file-p literate-config-org-file-name literate-config-el-file-name))
+    (literate-config-compile)))
+
+;;;###autoload
+(defun literate-config-load ()
+  "Load the config."
+  (load literate-config-el-file-name nil 'nomessage 'nosuffix))
+
+(defun literate-config-after-save-hook ()
+  "Compile the config when saved."
+  (when (string= literate-config-org-file-name buffer-file-name)
+    (literate-config-compile)))
+
+(defun literate-config-add-after-save-hook ()
+  "Install hook that compiles the config on save."
+  (add-hook 'after-save-hook 'literate-config-after-save-hook))
+
+;;;###autoload
+(defun literate-config-start ()
+  "Compile, install hooks, and load the config."
+  (literate-config-ensure-compiled)
+  (literate-config-add-after-save-hook)
+  (literate-config-load))
 
 (provide 'literate-config)
 ;;; literate-config.el ends here
